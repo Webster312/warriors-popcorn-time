1b6de02ea8c13f7f961168446a101f40651239c2
Christoph Weber <WeberChristoph85@gmail.com>
2023-04-05 15:42:32 +0200

    React exercise codealong

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,57 +1,35 @@
 import { useState } from "react";
-import moviesFromJson from "../data/movies.json";
-import Movie from './Movie.js'
+import Movie from "./Movie";
+
 import "./Main.css"
 
-function Main(){
-
-    const [moviesArr, setMoviesArr] = useState(moviesFromJson);
-    
-    const deleteMovie = (movieId) => {
-
-        // const newList = moviesArr.filter( (movieDetails) => {
-        //     if(movieDetails.id === movieId){
-        //         return false; //this movie will not be included in the new array
-        //     } else {
-        //         return true;
-        //     }
-        // });
-
-        const newList = moviesArr.filter( movieDetails => movieDetails.id !== movieId );
+function Main(props){
 
 
-        setMoviesArr(newList);
-    }
+    console.log(props)
 
 
-    let title;
-    if(moviesArr.length > 1){
-        title = <h1>We have {moviesArr.length} movies</h1>;
-    } else if(moviesArr.length === 1){
-        title = <h1>We have 1 movie</h1>;
-    } else {
-        title = <h1>Sorry, no movies to display</h1>;
-    }
+    // let title;
+    // if(moviesArr.length > 1){
+    //     title = <h1>We have {moviesArr.length} movies</h1>;
+    // } else if(moviesArr.length === 1){
+    //     title = <h1>We have 1 movie</h1>;
+    // } else {
+    //     title = <h1>Sorry, no movies to display</h1>;
+    // }
 
-    const sortByHighestRating = () => {
-        const copyArr = [...moviesArr].sort((a, b) => b.rating - a.rating);
-        setMoviesArr(copyArr);
-    }
 
-    const sortByLowestRating = () => {
-        const copyArr = [...moviesArr].sort((a, b) => a.rating - b.rating);
-        setMoviesArr(copyArr);
-    }
+    return(
+        <div className="Main">
+            
+            {/* <h1>We have {moviesArr.length} movies</h1> */}
 
-    return (
-      <div className="Main">
-        <button onClick={() => sortByHighestRating()}>SORT HIGHEST</button>
-        <button onClick={() => sortByLowestRating()}>SORT Lowest</button>
-        {moviesArr.map((movieObj) => {
-          return <Movie movieDetails={movieObj} parentDeleteFunction={deleteMovie} />;
-        })}
-      </div>
-    );
+            {props.moviesArr.map( (movieObj) => {
+                return <Movie key={movieObj.id} movieDetails={movieObj} callbackToDelete={props.callbackToDelete}  />
+            })}
+
+        </div>
+    )
 }
 
 export default Main;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,23 @@
-const Movie = (props) => {
-  return (
-    <div key={props.movieDetails.id} className="card">
-      <h2>{props.movieDetails.title}</h2>
+function Movie(props) {
 
-      {props.movieDetails.imgURL ? (
-        <img src={props.movieDetails.imgURL} alt={props.movieDetails.title} />
-      ) : (
-        <img src="https://dummyimage.com/182x268/aaaaaa/000000" />
-      )}
+    return (
+        <div className="card">
+            <h2>{props.movieDetails.title}</h2>
 
-      <h3>Rating: {props.movieDetails.rating}</h3>
-      <h3>Year: {props.movieDetails.year}</h3>
-      <button onClick={() => props.parentDeleteFunction(props.movieDetails.id)}>DELETE</button>
-      {props.movieDetails.rating > 8 && <p className="badge">RECOMMENDED</p>}
-    </div>
-  );
-};
+            {props.movieDetails.imgURL
+                ? <img src={props.movieDetails.imgURL} alt={props.movieDetails.title} />
+                : <img src="https://dummyimage.com/182x268/aaaaaa/000000" />
+            }
+
+            <h3>Rating: {props.movieDetails.rating}</h3>
+            <h3>Year: {props.movieDetails.year}</h3>
+
+            {props.movieDetails.rating > 8 && <p className="badge">RECOMMENDED</p>}
+
+
+            <button onClick={() => {props.callbackToDelete(props.movieDetails.title)}}>Delete</button>
+        </div>
+    )
+}
 
 export default Movie;
